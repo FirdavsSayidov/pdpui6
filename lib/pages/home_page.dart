35dfe807aff0821a975ebbf4b9905e3fe7106f23
acer <fsaidov167@gmail.com>
2022-08-22 17:17:26 +0500

    pdpui6

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(height: double.infinity,
+        child: Image(
+          image: AssetImage('assets/images/img.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
