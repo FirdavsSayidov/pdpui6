cd6d0626d58c17e9c514afc8671ea37f0ede16bb
acer <fsaidov167@gmail.com>
2022-08-22 22:43:19 +0500

    5-m 7-m

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:pdpui6/animations/FadeAnimation.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isLogin = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(height: double.infinity,
-        child: const Image(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/img.png',),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: Container(padding: EdgeInsets.only(left: 30,right: 30,top: 130,bottom: 30),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              colors: [
+                Colors.black.withOpacity(0.6),
+                Colors.black.withOpacity(0.5),
+                Colors.black.withOpacity(0.2)
+              ]
+            )
+          ),
+         child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           children: [
+             Column(
+               children: [
+                 FadeAnimation(1,
+                   Text('Find the best parties near you.',style: TextStyle(color: Colors.yellow.withOpacity(1),fontWeight: FontWeight.w400,fontSize: 40,),),
+                 ),
+                 SizedBox(height: 20,),
+                 FadeAnimation(1,
+                  Text('Let us find you a tutorial for your interest',style: TextStyle(color: Colors.green.withOpacity(1),fontWeight: FontWeight.w500,fontSize: 25),),
+                 ),
+               ]
+             ),
 
-          image: AssetImage('assets/images/img.png'),
-          fit: BoxFit.cover,
+               isLogin ?
+             Container(
+               height: 45,
+               decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.yellow[900],),
+               child: Center(
+                 child: Text('Start',style: TextStyle(color: Colors.white,fontSize: 17),),
+               ),
+             )
+             :
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.red,),
+                    child: Center(
+                      child: Text('Google',style: TextStyle(color: Colors.white,fontSize: 17),),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.blue,),
+                    child: Center(
+                      child: Text('Facebook',style: TextStyle(color: Colors.white,fontSize: 17),),
+                    ),
+                  ),
+                )
+              ],
+            )
+           ],
+         ),
         ),
       ),
     );
